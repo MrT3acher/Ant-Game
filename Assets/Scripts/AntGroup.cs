6f3a5cdfb1fe6fb5301ed603fcfcebbff5041ce1
hamidreza moallem taheri <hr.mt2000@gmail.com>
2019-10-03 11:18:47 +0330

    Start Programming

---
+++ Assets/Scripts/AntGroup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntGroup : MonoBehaviour
+{
+    public int count;
+
+    [SerializeField]
+    private int id;
+
+
+    public void Player1Move()
+    {
+
+    }
+    public void Player2Move()
+    {
+
+    }
+
+    public void Increase(int value)
+    {
+
+    }
+
+    public void Decrease(int value)
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
