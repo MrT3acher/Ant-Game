319d0ed6454117d4725aa77677e233c4cc3a0d35
f7899 <fz1999.ja@gmail.com>
2019-10-03 02:43:50 -0700

    gh

--- Assets/Scripts/AntGroup.cs
+++ Assets/Scripts/AntGroup.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class AntGroup : MonoBehaviour
 {
     public int count;
-
+    
     public void Increase(int value)
     {
-
+        count += value;
+            
+       
+    }
+    void OnCollisionEnter(Collision2D col)
+    {
+       
     }
 
     public void Decrease(int value)
     {
-
+       
     }
 }
