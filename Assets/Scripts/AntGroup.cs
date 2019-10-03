9142c2d40d171b93a22cfcbdfbf3961021898812
hamidreza moallem taheri <hr.mt2000@gmail.com>
2019-10-03 11:42:45 +0330

    Change Software Desing

--- Assets/Scripts/AntGroup.cs
+++ Assets/Scripts/AntGroup.cs
@@ -6,19 +6,6 @@ public class AntGroup : MonoBehaviour
 {
     public int count;
 
-    [SerializeField]
-    private int id;
-
-
-    public void Player1Move()
-    {
-
-    }
-    public void Player2Move()
-    {
-
-    }
-
     public void Increase(int value)
     {
 
@@ -27,10 +14,5 @@ public class AntGroup : MonoBehaviour
     public void Decrease(int value)
     {
 
-    }
-
-    void Update()
-    {
-        
     }
 }
